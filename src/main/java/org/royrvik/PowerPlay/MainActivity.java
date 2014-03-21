fa3a2bb66297fbf527b81490a7e0d7b074399fb9
andrroy <andreas@royrvik.org>
2014-03-21 23:35:25 +0100

    Added Welcome view + blank gaming view

--- src/main/java/org/royrvik/PowerPlay/MainActivity.java
+++ src/main/java/org/royrvik/PowerPlay/MainActivity.java
@@ -1,17 +1,31 @@
 package org.royrvik.PowerPlay;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import utils.ParseXML;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private Button playbutton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        double test = ParseXML.getLatestValue();
+
+        playbutton = (Button) findViewById(R.id.playButton);
+
+        playbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(i);
+            }
+        });
+        //double test = ParseXML.getLatestValue();
+        //Log.d("APP", Double.toString(test));
     }
 }
---
+++ src/main/java/org/royrvik/PowerPlay/PlayActivity.java
@@ -0,0 +1,13 @@
+package org.royrvik.PowerPlay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andrroy on 21/03/14.
+ */
+public class PlayActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
