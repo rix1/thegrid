9c8699bf5483cfd3bc644c35490f48d9f8d90bed
andrroy <andreas@royrvik.org>
2014-03-22 02:12:03 +0100

    Added graphics

--- src/main/java/org/royrvik/PowerPlay/MainActivity.java
+++ src/main/java/org/royrvik/PowerPlay/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 
@@ -14,6 +16,11 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.main);
 
         playbutton = (Button) findViewById(R.id.playButton);
--- src/main/java/org/royrvik/PowerPlay/SplashScreenActivity.java
+++ src/main/java/org/royrvik/PowerPlay/SplashScreenActivity.java
@@ -4,16 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-/**
- * Created by andrroy on 21/03/14.
- */
+
 public class SplashScreenActivity extends Activity {
 
     private static int SPLASH_TIME_OUT = 3000;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.splashscreen);
 
         new Handler().postDelayed(new Runnable() {
